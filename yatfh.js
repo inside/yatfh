d7e57e831548e04527a6ccda09440107428dd8c2
Yann Thomas-Gerard <yann.thomas-gerard@dailymotion.com>
2014-09-08 17:10:24 +0200

    Fixes css and implements the table wrapping method

--- yatfh.js
+++ yatfh.js
@@ -1,6 +1,17 @@
 (function($) {
     $.fn.yatfh = function() {
-        console.log('this is yatfh');
+        if (this.hasClass('table-bordered')) {
+            this.removeClass('table-bordered');
+        }
+
+        this.wrap('<div class="yatfh"><div class="yatfh-inner"></div></div>');
+        this.find('thead th').each(function() {
+            var $this = $(this);
+            var html = '<div class="yatfh-th-for-width-constraint">' + $this.html() + '</div>';
+                html += '<div class="yatfh-th-displayed">' + $this.html() + '</div>';
+            $this.html(html);
+        });
+
         return this;
     };
-}(jQuery));
+} (jQuery));
